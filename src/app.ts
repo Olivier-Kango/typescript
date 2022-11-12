ebe7f63256ebce209da3c96458e016859f61030f
Olivier-Kango <olivierkango@gmail.com>
2022-11-12 09:03:39 -0800

    Delete app.js in src

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-console.log('Hello')
+// const a: string = "Hello"
+// const n: number = 3
+// const b: boolean = true
+// const d: null = null
+// const arr: any[] = ['aze', 'aze', 3]
+// const user: {firstname: string, [key: string]: string} = {firstname: "John", lastname: "Doe"}
+// const date: Date = new Date();
+// const cb: (e: MouseEvent) => void = (e: MouseEvent) => {
+//   return 3
+// }
+
+// function printId(id: number | string): void {
+//   console.log(id.toString)
+// }
+
+
+const compteur = document.querySelector('#compteur');
+
+let i = 0;
+
+const increment = (e: Event) => {
+  e.preventDefault();
+  i++;
+
+  const span = compteur?.querySelector('span')
+  if (span) {
+    span.innerText = i.toString()
+  }
+}
+
+compteur?.addEventListener('click', increment);
+
